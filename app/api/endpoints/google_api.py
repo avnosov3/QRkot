fb4ef809bcd85c527c15b82a38d9da775fd2bbc7
Artem Nosov <nosov1995@gmail.com>
2023-04-21 12:39:26 +0200

    add update-google-sheet

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -7,7 +7,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 from app.core.google_client import get_service
 from app.crud.charity_project import charity_crud
-from app.services.google_api import spreadsheets_create
+from app.services.google_api import (
+    spreadsheets_create, set_user_permissions, spreadsheets_update_value
+)
 from app.schemas.charity_project import CharityDB
 
 
@@ -21,7 +23,13 @@ router = APIRouter()
 )
 async def get_all_invested(
     session: AsyncSession = Depends(get_async_session),
-    wrapper_servises: Aiogoogle = Depends(get_service)
+    wrapper_services: Aiogoogle = Depends(get_service)
 ):
-    await spreadsheets_create(wrapper_servises)
-    return await charity_crud.get_projects_by_completion_rate(session)
+    projects = await charity_crud.get_projects_by_completion_rate(session)
+    spreadsheet_id = await spreadsheets_create(wrapper_services)
+    await set_user_permissions(spreadsheet_id, wrapper_services)
+    await spreadsheets_update_value(
+        spreadsheet_id, projects, wrapper_services
+    )
+    return projects
+    # return f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}'
--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from typing import List
 
 from aiogoogle import Aiogoogle
 
 from app.core.config import settings
+from app.models.charity_project import CharityProject
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
 OUTUPUT_DATE = 'Отчёт на {}'
@@ -36,3 +38,55 @@ async def spreadsheets_create(wrapper_services: Aiogoogle):
     spreadsheet_id = response['spreadsheetId']
     print('https://docs.google.com/spreadsheets/d/' + spreadsheet_id)
     return spreadsheet_id
+
+
+async def set_user_permissions(
+    spreadsheet_id: str,
+    wrapper_services: Aiogoogle
+):
+    body = {
+        'type': 'user',
+        'role': 'writer',
+        'emailAddress': settings.email
+    }
+    service = await wrapper_services.discover('drive', 'v3')
+    await wrapper_services.as_service_account(
+        service.permissions.create(
+            fileId=spreadsheet_id,
+            json=body,
+            fields='id'
+        )
+    )
+
+
+async def spreadsheets_update_value(
+    spreadsheet_id: str,
+    chatiry_projects: List[CharityProject],
+    wrapper_services: Aiogoogle
+):
+    now = datetime.now().strftime(FORMAT)
+    service = await wrapper_services.discover('sheets', 'v4')
+    table_values = [
+        ['Отчет от ', now],
+        ['Топ проектов по скорости закрытия'],
+        ['Название проекта', 'Время сбора', 'Описание']
+    ]
+    for project in chatiry_projects:
+        row = [
+            str(project.name),
+            str(project.close_date - project.create_date),
+            str(project.description),
+        ]
+        table_values.append(row)
+    body = {
+        'majorDimension': 'ROWS',
+        'values': table_values,
+    }
+    await wrapper_services.as_service_account(
+        service.spreadsheets.values.update(
+            spreadsheetId=spreadsheet_id,
+            range='A1:E30',
+            valueInputOption='USER_ENTERED',
+            json=body,
+        )
+    )
